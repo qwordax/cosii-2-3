44ba6cb07a4ea1c763047754e2b51c98ffd11f92
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-11-05 13:57:02 +0300

    `feat:` implement basic image showing

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import cv2 as cv
+
 import sys
 
 def main():
     '''
     The main function of the program.
     '''
+    path = input('path: ')
+
+    image = cv.imread(path)
+
+    cv.imshow('Initial Image', image)
+
+    # Waiting for user.
+    cv.waitKey(0)
+
     sys.exit(0)
 
 if __name__ == '__main__':
