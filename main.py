680b1e06af5bcbfea8938c70f7b6a0d01527dfde
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-11-07 19:20:36 +0300

    `feat:` add dummy `wavelet_transform`

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import cv2 as cv
 
 import sys
 
+def wavelet_transform(image):
+    '''
+    Performs the wavelet transform of an `image`.
+    '''
+    pass
+
 def main():
     '''
     The main function of the program.
     '''
     path = input('path: ')
 
-    image = cv.imread(path)
+    image = cv.imread(path, cv.IMREAD_UNCHANGED)
 
     cv.imshow('Initial Image', image)
 
